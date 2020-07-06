118d9286bdf8f33d0e4165e624fdeb6f6d5ae929
Cody Grainger <cwgrainger1s@semo.edu>
2020-07-06 09:23:30 -0500

    Finished Part 1

---
+++ hw02/hw02-1.R
@@ -0,0 +1,92 @@
+#Cody Grainger
+
+#HW02 Part 1: Intro to Basics
+
+## Section 1.1
+
+#Addition
+30+12
+
+## Section 1.2
+
+# Division
+121/1.1
+
+# Exponentiation
+2^5
+
+# Modulo
+28 %% 6
+
+# Exponentiation without parentheses
+49^1/2
+49^(1/2)
+
+## Section 1.3 Variable assignment
+
+# Stored Variables
+my_var <- 4
+my_var
+
+x <- 42
+x
+
+d_merriami <- 24
+d_ordii <- 9
+d_merriami
+d_ordii
+
+## Section 1.4 Working with variables
+
+#Correcting the mistake
+d_merriami + 5
+d_merriami
+
+d_merriami <- d_merriami + 5
+
+# Add together the number of individuals of each species.
+# Results stored in 'total_k_rats'
+total_k_rats <- d_merriami + d_ordii
+total_k_rats
+
+area_sampled <- 1000
+area_sampled
+
+density <- total_k_rats / area_sampled
+density
+
+## Section 1.5 Basic data types in R
+
+# Numerics (decimal values)
+4.5
+
+# Natural (Integers, Integers are also numerics)
+4
+
+# Boolean values are logical
+TRUE
+FALSE
+True <- "this value is not boolean"
+
+# Text (string) in "" marks
+"my text string"
+
+#Example
+numeric_var <- 42
+
+species_name <- "Dipodomys merriami"
+
+is_mammal <- TRUE
+
+## Section 1.6 What is the data type?
+
+# Checking class of 'numeric_var'
+class(numeric_var)
+
+# Checking class of 'species_name'
+class(species_name)
+
+#Checking class of 'is_mammal'
+class(is_mammal)
+
+#
